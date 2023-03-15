208c73ea50fb6cbe526c2c323b68e47bcd4c7a71
franjavi01 <36012280+franjavi01@users.noreply.github.com>
2023-03-15 20:50:21 +0100

    Empresa X

---
+++ script.js
@@ -0,0 +1,7 @@
+function dia() {
+    document.getElementById("diaNoche").style="background:white; color:black;"
+    }
+    
+    function noche() {
+    document.getElementById("diaNoche").style="background:black; color:white;"
+    }
